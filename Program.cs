6a1c5d18cda8c1080887c5b674903acbae66155c
Alexandr Kuzmin <kuzmin_89@mail.ru>
2023-01-29 22:49:25 +0500

    Убрал жёлтые линии

--- Program.cs
+++ Program.cs
@@ -11,14 +11,14 @@ Console.WriteLine($"Начальный массив array1: [{string.Join(", ", 
 // Определяем длину 2 массива, посчитав количество элементов <= 3
 for (int i = 0; i < array1.Length; i++)
 {
-    if (array1[i].Length <= dlina)
+    if (array1[i]?.Length <= dlina)
         count++;
 }
 string? [] array2 = new string[count];
 // Заполняем 2 массив
 for (int i = 0, j = 0; i < array1.Length; i++)
 {
-    if (array1[i].Length <= dlina)
+    if (array1[i]?.Length <= dlina)
         {
             array2[j] = array1[i];
             j++;
