ebce8866c288c365ca2fc8c19e7d4a71f4be9aad
Alexandr Kuzmin <kuzmin_89@mail.ru>
2023-01-29 22:47:58 +0500

    programma

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите количество элементов массива: ");
+﻿Console.Clear();
+Console.WriteLine("Введите количество элементов массива: ");
 int size = Convert.ToInt32(Console.ReadLine()), count = 0, dlina = 3;
 string? [] array1 = new string[size];
 for (int i = 0; i < array1.Length; i++)
@@ -17,14 +18,10 @@ string? [] array2 = new string[count];
 // Заполняем 2 массив
 for (int i = 0, j = 0; i < array1.Length; i++)
 {
-    while (j < count)
-    {
-        if (array1[i].Length <= dlina)
-            {
-                array2[j] = array1[i];
-                j++;
-            }
-        break;
-    }
+    if (array1[i].Length <= dlina)
+        {
+            array2[j] = array1[i];
+            j++;
+        }
 }
 Console.Write($"array2: [{string.Join(", ", array2)}]");
